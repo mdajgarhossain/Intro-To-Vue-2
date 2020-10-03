9be3e0f1c9fad877105130c16e640d273392db96
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-10-03 23:04:12 +0600

    Create vue instance

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const rootElement = document.getElementById("app");
-console.log(rootElement);
-const element = document.createElement("h1");
-element.textContent = "Product goes here";
-rootElement.appendChild(element);
-
-// var product = "Socks";
-var product = document.createTextNode("Socks");
-rootElement.appendChild(product);
+var app = new Vue({
+  el: "#app",
+  data: {
+    heading: "Product goes here",
+    product: "Socks",
+  },
+});
