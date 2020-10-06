3e7c747b820a393badb1019cb78fafb7894433e5
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-10-06 15:00:42 +0600

    Intro to component

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
+var data = {
+  name: "Javascript",
+};
+
+Vue.component("greeting", {
+  template:
+    "<p>Hey there, I am {{name}}. <button v-on:click='changeName'>Change name</button></p>",
+  //data as a function for creating individual data object(reference) for every instance
+  /*data() {
+    return {
+      name: "Javascript",
+    };
+  },*/
+  //updating all instances data together by passing above global 'data' variable
+  data() {
+    return data;
+  },
+  methods: {
+    changeName() {
+      this.name = "vueJS";
+    },
+  },
+});
+
 //First instance stored in variable one
 var one = new Vue({
   el: "#vue-app-one",
-  data: {
-    title: "Vue App One",
-  },
-  methods: {},
-  computed: {
-    greet() {
-      return "Hello from app one!";
-    },
-  },
+  data: {},
 });
 
 //Second instance stored in variable two
 var two = new Vue({
   el: "#vue-app-two",
-  data: {
-    title: "Vue App Two",
-  },
-  methods: {
-    //interaction between both instances
-    changeTitle() {
-      one.title = "Title has changed from app two";
-    },
-  },
-  computed: {
-    greet() {
-      return "Hello from app two!";
-    },
-    seeChange() {
-      return one.title;
-    },
-  },
+  data: {},
 });
